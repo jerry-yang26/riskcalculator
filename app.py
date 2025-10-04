348a2ea94c8f7f77ea864cd7d6d5b2e2f546eb40
jerry-yang26 <jerry.yang.app@gmail.com>
2025-10-04 10:19:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,9 @@ def calculate_risk():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
